6afce69e4584aa40a544967717af1a6eea0b6fda
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-06-28 03:41:59 -0700

    Assignment 142

---
+++ Basic_C#_Programs/Assign142/Assign142/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assign142
+{
+    class Program
+    {
+        public static bool DUI { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance Approval Form");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string ageS = Console.ReadLine();
+            int age = Convert.ToInt16(ageS);
+
+            Console.WriteLine("Have you ever had a DUI? Please answer \"Yes\" or \"No\"");
+            string DUIs = Console.ReadLine();
+
+
+            if (DUIs == "Yes")
+                {
+                     DUI = true;
+                }else if (DUIs == "No")
+                {
+                     DUI = false;
+                }else
+                {
+                    Console.WriteLine("Please answer \"Yes\" or \"No\"");
+                }
+
+            Console.WriteLine("How many Speeding tickets do you have?");
+            string sTixS = Console.ReadLine();
+            int STix = Convert.ToInt16(sTixS);
+
+            bool INS = (age > 15) && DUI == false && STix <= 3;
+            if (INS == true)
+            {
+                Console.WriteLine("You qualify for Insurance.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, You do not qualify for Insurance");
+            }
+        }
+    }
+}
